e23275bfb1582df58d4452dfe7d59325c8ccb706
allencai11 <152452752+allencai11@users.noreply.github.com>
2024-07-05 15:16:00 -0500

    ds

--- Controls-Camp-2024/src/main/java/frc/robot/Constants.java
+++ Controls-Camp-2024/src/main/java/frc/robot/Constants.java
@@ -89,7 +89,6 @@ public final class Constants {
   }
 
   public final class LEDConstants {
-    public static final int LEDBufferLen = 140;
 
     public enum States {
       DEFAULT,
--- Controls-Camp-2024/src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ Controls-Camp-2024/src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -15,7 +15,7 @@ public class LEDSubsystem extends SubsystemBase {
     public LEDSubsystem(int p_port) {
         m_port = p_port;
         m_addressableLED = new AddressableLED(m_port);
-        m_LEDBuffer = new AddressableLEDBuffer(LEDConstants.LEDBufferLen);
+        m_LEDBuffer = new AddressableLEDBuffer();
         m_addressableLED.setLength(m_LEDBuffer.getLength());
         m_addressableLED.setData(m_LEDBuffer);
         m_addressableLED.start();
@@ -55,16 +55,6 @@ public class LEDSubsystem extends SubsystemBase {
 
     public void setToCyan() {
         setToColor(0, 255, 255);
-    }
-
-    // false = broken
-    public void sarahStrobe() {
-        double timeConst = Math.PI;
-        int r = (int) Math.round(126 * Math.cos(timeConst / 4 * timer.get()) + 126);
-        int g = (int) Math.round(126 * Math.cos(timeConst / 8 * timer.get()) + 126);
-        int b = (int) Math.round(126 * Math.sin(timeConst / 2 * timer.get()) + 126);
-        for (int i = 0; i < m_LEDBuffer.getLength(); ++i)
-            setToColor(r, g, b);
     }
 
     public void periodic() {
