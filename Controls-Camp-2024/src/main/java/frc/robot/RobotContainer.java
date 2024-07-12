eaa0af2de6da6947ac6d6c140f5c49babe5ced5d
Vienna Rossmanith <123705736+ViennaRossmanith@users.noreply.github.com>
2024-07-12 15:27:04 -0500

    constants trimmed

--- Controls-Camp-2024/src/main/java/frc/robot/RobotContainer.java
+++ Controls-Camp-2024/src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,14 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
 
-  CommandXboxController m_buttonsController = new CommandXboxController(OperatorConstants.XBOX_CONTROLLER);
-  CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER);
+  CommandXboxController m_buttonsController = new CommandXboxController(1);
+  CommandXboxController m_driverController = new CommandXboxController(0);
 
   public RobotContainer() {
     configureBindings();
