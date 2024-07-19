1ae273cc42e1bb1a95b5d8f85ac059b38334f031
Noah9847 <70857168+Noah9847@users.noreply.github.com>
2024-07-19 17:35:52 -0500

    L1

--- Controls-Camp-2024/src/main/java/frc/robot/util/SubsystemContainer.java
+++ Controls-Camp-2024/src/main/java/frc/robot/util/SubsystemContainer.java
@@ -1,18 +1,17 @@
 package frc.robot.util;
 
 import frc.robot.Constants.PWMConstants;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.LEDSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
-import frc.robot.subsystems.wrapper.Arm;
-import frc.robot.subsystems.wrapper.Intake;
-import frc.robot.subsystems.wrapper.NetworkTables;
-import frc.robot.subsystems.wrapper.Shooter;
 
 public class SubsystemContainer {
-    public static final NetworkTables sendables = new NetworkTables();
+    // public static final NetworkTables sendables = new NetworkTables();
     public static final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
-    public static final Intake intakeSubsystem = new Intake();
-    public static final Shooter shooterSubsystem = new Shooter();
-    public static final Arm armSubsystem = new Arm();
+    public static final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+    public static final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
+    public static final ArmSubsystem armSubsystem = new ArmSubsystem();
     public static final LEDSubsystem LEDSubsystem = new LEDSubsystem(PWMConstants.LED1);
 }
