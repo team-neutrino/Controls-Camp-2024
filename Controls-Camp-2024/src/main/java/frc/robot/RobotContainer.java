1589d96887a9edbe34fcf97d58691ff31471c4e1
Vienna Rossmanith <123705736+ViennaRossmanith@users.noreply.github.com>
2024-07-18 23:25:03 -0500

    ?

--- Controls-Camp-2024/src/main/java/frc/robot/RobotContainer.java
+++ Controls-Camp-2024/src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.IntakeDefaultCommand;
+import frc.robot.commands.LEDefaultCommand;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.LEDSubsystem;
 
@@ -17,18 +18,21 @@ public class RobotContainer {
   IntakeSubsystem m_intakeSubsystem;
 
   LEDSubsystem m_LEDSubsystem;
+  LEDefaultcommand m_LEDefaultCommand;
 
   public RobotContainer() {
     m_intakeDefaultCommand = new IntakeDefaultCommand();
     m_intakeSubsystem = new IntakeSubsystem();
 
     m_LEDSubsystem = new LEDSubsystem(0);
+    m_LEDefaultCommand = new LEDefaultCommand();
 
     configureBindings();
   }
 
   private void configureBindings() {
     m_intakeSubsystem.setDefaultCommand(m_intakeDefaultCommand);
+    m_LEDSubsystem.setDefaultCommand(m_LEDefaultCommand);
 
     // Intake buttons
 
